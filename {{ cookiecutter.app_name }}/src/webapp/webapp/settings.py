8e6907f489e6dcb134eb68a920471a7ace7b2b9f
Cheukting <cheukting.ho@gmail.com>
2023-05-23 15:07:09 +0100

    user generate their own secret key

--- {{ cookiecutter.app_name }}/src/webapp/webapp/settings.py
+++ {{ cookiecutter.app_name }}/src/webapp/webapp/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-86kqbg4s&53!bmq5)pd0hg@a%7b1pn_7&yw(tljy=xh12*!_u$'  # noqa: E501
+# Generate your secret key
+# See https://humberto.io/blog/tldr-generate-django-secret-key/
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
